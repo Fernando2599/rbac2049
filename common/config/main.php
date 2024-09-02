bc3031d001b8cc18df82e87bf6ffa4e8e09aa3dd
Fernando Jose Uc Poot <fernandopoot247@gmail.com>
2024-09-02 08:20:45 -0600

    Solucionando choque de Version de Boostrap

--- common/config/main.php
+++ common/config/main.php
@@ -7,7 +7,13 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => \yii\caching\FileCache::class,
+            'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+        ]
+    ]
+    
 ];
